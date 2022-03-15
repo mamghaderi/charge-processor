f31c0a0fdbfacde5d067c2aadbc644cdeae0720c
serveh.mamghaderi <serveh.mamghaderi@pushe.co>
2022-03-15 04:43:07 +0330

    add description to on_post

--- charge/api.py
+++ charge/api.py
@@ -41,6 +41,36 @@ class Rate(object):
 
     @jsonschema.validate(req_schema=rate_request_schema)
     def on_post(self, req, resp):
+        """
+        :param req: a json contains rate and cdr objects,
+            and should be pass ``rate_request_schema`` format.
+            Example:
+                .. code:: python
+                        {
+                            "rate": {
+                                "energy": 0.3, # -- float --
+                                "time": 2, # -- float --
+                                "transaction": 1 # -- float --
+                            },
+                            "cdr": {
+                                "meterStart": 1204307, # -- Integer --
+                                "timestampStart": "2021-04-05T10:04:00Z", # -- timestamp(ISO 8601) string --
+                                "meterStop": 1215230, # -- Integer --
+                                "timestampStop": "2021-04-05T11:27:00+00:00" # -- timestamp(ISO 8601) string --
+                            }
+                        }
+        :param resp: a json contains components and overall prices
+            Example:
+                .. code:: python
+                        {
+                            "components": {
+                                "energy": 3.277, # -- float with maximum 3 decimal places --
+                                "time": 2.767, # -- float with maximum 3 decimal places --
+                                "transaction": 1 # -- float with maximum 3 decimal places --
+                            },
+                            "overall": 7.04 # -- float with maximum 3 decimal places --
+                        }
+        """
         data = req.media
         resp.text = json.dumps(process_rate(data['rate'], data['cdr']))
         resp.status = falcon.HTTP_OK
